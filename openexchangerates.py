967e7ba263f70f28d095073a3199194cdda9f877
Camillus Gerard Cai <cgcai@qxcg.net>
2015-06-02 13:10:55 +0800

    Initial commit.

---
+++ openexchangerates.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class OpenExchangeRates(object):
+    __base_url = 'http://openexchangerates.org/api'
+
+    def __init__(self, api_key):
+        self.__api_key = api_key
+
+    def __del__(self):
+        pass
+
+    def currencies(self):
+        url = '{base}/currencies.json?app_id={app_id}'.format(
+            base=OpenExchangeRates.__base_url, app_id=self.__api_key)
+
+        # TODO: no network/http error status codes
+        r = requests.get(url)
+        return r.json()
+
+    def latest(self, base_currency='SGD'):
+        url = '{base}/latest.json?base={currency}&app_id={app_id}'.format(
+            base=OpenExchangeRates.__base_url, currency=base_currency,
+            app_id=self.__api_key)
+
+        # TODO: no network/http error status codes
+        r = requests.get(url)
+        return r.json()
+
+    # TODO: implement
+    # def historical(self, year, month, day):
+    #    pass
